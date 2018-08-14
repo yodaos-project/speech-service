52e9b83d328bd2656613369ff90db0477e461106
chen.zhang <chen.zhang@rokid.com>
2018-08-14 21:06:44 +0800

    vsys编译通过

---
+++ vsys.mk
@@ -0,0 +1,12 @@
+local-path := $(call my-dir)
+
+include $(clear_vars)
+local.module := speech-service
+local.ndk-modules := speech-service.ndk
+include $(build-executable)
+
+include $(clear-vars)
+local.module := speech-service.ndk
+local.ndk-script := $(local-path)/ndk.mk
+local.ndk-modules := flora speech
+include $(build-ndk-module)
